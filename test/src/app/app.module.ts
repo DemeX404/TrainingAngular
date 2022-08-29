52779e6da904b6f5bb0c37cc9c2585325779e782
DemeX404 <damexica@gmail.com>
2022-08-29 15:27:22 +0200

    Binding in all directions

--- test/src/app/app.module.ts
+++ test/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeComponent } from './employe/employe.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent, EmployeesComponent, EmployeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- test/src/app/employe/employe.component.ts
+++ test/src/app/employe/employe.component.ts
@@ -9,8 +9,27 @@ export class EmployeComponent implements OnInit {
 
   name="Juan"
   surname="Díaz"
-  age =20;
-  //company="Woxter"
+  age = 20;
+  company = "Google";
+
+  propertySquare = false;
+  propertyLog=false;
+  textRegister = "Nobody is registered";
+
+  getLogUsers(){
+    this.propertyLog=false;
+  }
+
+  setRegisterUser(event:Event){
+   // alert("User Registred");
+
+   if((<HTMLInputElement>event.target).value=="yes"){
+    this.textRegister = "User Registered"
+   }else{
+    this.textRegister = "User is not Registered"
+   }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
